749029f9aaacf948d45683c37c2b6e94e5ec9fde
Phil <philbroderick12@gmail.com>
2019-11-06 20:46:26 +0000

    Add Core class library and ShowAPI interface and move some files

---
+++ TeleTracker.BLL/Services/TheMovieDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TeleTracker.BLL.Interfaces;
+using TeleTracker.Core.Common;
+using TeleTracker.Core.DTOs;
+
+namespace TeleTracker.BLL.Services
+{
+    public class TheMovieDB : IShowAPI
+    {
+        public Task<List<ShowDTO>> GetShowsBySearchTerm(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ShowDTO>> GetTopShowsByCriteria(CriteriaValueEnum citeria, TimeSpan timeSpan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ShowDTO>> GetUserSuscribedShowsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TeleTracker.Core/Common/CriteriaValueEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace TeleTracker.Core.Common
+{
+    public enum CriteriaValueEnum
+    {
+        Top = 0,
+        New = 1,
+        Best = 2
+    }
+}
